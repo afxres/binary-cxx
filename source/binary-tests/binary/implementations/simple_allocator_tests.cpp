2af6836e154be1a40c94de6cbbd7d91ec3a9752d
miko <stdarg@outlook.com>
2019-11-25 16:10:18 +0800

    Add debugger display file

--- source/binary-tests/binary/implementations/simple_allocator_tests.cpp
+++ source/binary-tests/binary/implementations/simple_allocator_tests.cpp
@@ -15,4 +15,15 @@ namespace mikodev::binary::tests::implementations::simple_allocator_tests
         BOOST_REQUIRE_EQUAL(INT32_MAX, allocator.max_capacity());
         BOOST_REQUIRE_EQUAL(nullptr, allocator.data());
     }
+
+    BOOST_AUTO_TEST_CASE(simple_allocator__constructor__max_capacity_little_than_capacity)
+    {
+        size_t capacity = 20;
+        auto data = std::shared_ptr<byte_t[]>(new byte_t[capacity]);
+        auto allocator = mki::simple_allocator(data, capacity, 10);
+        BOOST_REQUIRE_EQUAL(0, allocator.size());
+        BOOST_REQUIRE_EQUAL(10, allocator.capacity());
+        BOOST_REQUIRE_EQUAL(10, allocator.max_capacity());
+        BOOST_REQUIRE_EQUAL(data, allocator.data());
+    }
 }
