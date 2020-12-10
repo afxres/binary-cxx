2a19ab74264970aa6ff1e3eec65c41037b0e8c0f
miko <stdarg@outlook.com>
2020-12-10 23:16:15 +0800

    Fix endian type

--- code/binary/include/binary/endian.hpp
+++ code/binary/include/binary/endian.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#if defined(__linux__)
 #include <endian.h>
+#endif
+
+#if defined(_WIN32)
+#include <stdlib.h>
+#endif
 
 namespace mikodev::binary
 {
@@ -10,29 +16,47 @@ namespace mikodev::binary
     private:
         endian() = delete;
 
+#if defined(_WIN32)
+        template <typename R>
+        static inline R __cast(T item) { return *reinterpret_cast<R*>(&item); }
+#endif
+
     public:
         static T ensure_little_endian(T item)
         {
             if constexpr (sizeof(T) == 1)
                 return item;
+#if defined(__linux__)
             else if constexpr (sizeof(T) == 2)
                 return reinterpret_cast<T>(htole16(reinterpret_cast<uint16_t>(item)));
             else if constexpr (sizeof(T) == 4)
                 return reinterpret_cast<T>(htole32(reinterpret_cast<uint32_t>(item)));
             else if constexpr (sizeof(T) == 8)
                 return reinterpret_cast<T>(htole64(reinterpret_cast<uint64_t>(item)));
+#elif defined(_WIN32)
+            return item;
+#endif
         }
 
         static T ensure_big_endian(T item)
         {
             if constexpr (sizeof(T) == 1)
                 return item;
+#if defined(__linux__)
             else if constexpr (sizeof(T) == 2)
                 return reinterpret_cast<T>(htobe16(reinterpret_cast<uint16_t>(item)));
             else if constexpr (sizeof(T) == 4)
                 return reinterpret_cast<T>(htobe32(reinterpret_cast<uint32_t>(item)));
             else if constexpr (sizeof(T) == 8)
                 return reinterpret_cast<T>(htobe64(reinterpret_cast<uint64_t>(item)));
+#elif defined(_WIN32)
+            else if constexpr (sizeof(T) == 2)
+                return __cast<T>(_byteswap_ushort(__cast<uint16_t>(item)));
+            else if constexpr (sizeof(T) == 4)
+                return __cast<T>(_byteswap_ulong(__cast<uint32_t>(item)));
+            else if constexpr (sizeof(T) == 8)
+                return __cast<T>(_byteswap_uint64(__cast<uint64_t>(item)));
+#endif
         }
     };
 }
