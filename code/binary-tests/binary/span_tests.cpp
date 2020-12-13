66caa08e38fd017c79170989c9519b87408be35d
miko <stdarg@outlook.com>
2020-12-13 13:20:51 +0800

    Add debugger display comments for allocator and span

--- code/binary-tests/binary/span_tests.cpp
+++ code/binary-tests/binary/span_tests.cpp
@@ -12,4 +12,13 @@ namespace mikodev::binary::tests::span_tests
         BOOST_REQUIRE_EQUAL(span.buffer(), nullptr);
         BOOST_REQUIRE_EQUAL(span.length(), 0);
     }
+
+    BOOST_AUTO_TEST_CASE(span__constructor__unchecked_from_stack)
+    {
+        const char text[] = "Hello, world!";
+        mk::span span = mk::span(reinterpret_cast<const_byte_ptr>(&text[0]), sizeof(text) - 1);
+        BOOST_TEST_MESSAGE(reinterpret_cast<const char*>(span.buffer()));
+        BOOST_REQUIRE_EQUAL(reinterpret_cast<const char*>(span.buffer()), &text[0]);
+        BOOST_REQUIRE_EQUAL(span.length(), sizeof(text) - 1);
+    }
 }
