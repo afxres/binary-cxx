f6b1184f4d17ed172a3ff626f7ee73592978bdcc
miko <stdarg@outlook.com>
2025-01-18 15:19:12 +0800

    Add unit tests for tuple converter with default values

--- next/binary-tests/src/binary/converters/TupleConverterTests/TupleConverterTests.cpp
+++ next/binary-tests/src/binary/converters/TupleConverterTests/TupleConverterTests.cpp
@@ -48,5 +48,50 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(VariableLengthTupleConverterTest, T, VariableLengt
     BOOST_REQUIRE_EQUAL(0, converter->Length());
 }
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(ConstantLengthTupleConverterEncodeDecodeBothMethodsWithDefaultTupleValueTest, T, ConstantLengthTupleConverterTestTypeData) {
+    ::binary::Generator generator;
+    ::binary::AddConverter<::binary::converters::LittleEndianConverter<int16_t>>(generator);
+    ::binary::AddConverter<::binary::converters::LittleEndianConverter<int32_t>>(generator);
+    ::binary::AddConverter<::binary::converters::LittleEndianConverter<int64_t>>(generator);
+    ::binary::AddConverter<::binary::converters::TupleConverter<T>>(generator);
+    auto converter = ::binary::GetConverter<T>(generator);
+    auto dataBuffer = ::binary::Allocator::Invoke([converter](auto& allocator) { converter->Encode(allocator, T()); });
+    auto autoBuffer = ::binary::Allocator::Invoke([converter](auto& allocator) { converter->EncodeAuto(allocator, T()); });
+    BOOST_REQUIRE_EQUAL(converter->Length(), dataBuffer.size());
+    BOOST_REQUIRE_EQUAL(converter->Length(), autoBuffer.size());
+    BOOST_REQUIRE_EQUAL(std::string_view(reinterpret_cast<const char*>(dataBuffer.data()), dataBuffer.size()), std::string_view(reinterpret_cast<const char*>(autoBuffer.data()), autoBuffer.size()));
+
+    std::span<const std::byte> dataSpan(dataBuffer);
+    std::span<const std::byte> autoSpan(autoBuffer);
+    auto dataResult = converter->Decode(dataSpan);
+    auto autoResult = converter->DecodeAuto(autoSpan);
+    BOOST_REQUIRE(T() == dataResult);
+    BOOST_REQUIRE(T() == autoResult);
+    BOOST_REQUIRE_EQUAL(dataSpan.size(), converter->Length());
+    BOOST_REQUIRE_EQUAL(autoSpan.size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(VariableLengthTupleConverterEncodeDecodeBothMethodsWithDefaultTupleValueTest, T, VariableLengthTupleConverterTestTypeData) {
+    ::binary::Generator generator;
+    ::binary::AddConverter<::binary::converters::LittleEndianConverter<int16_t>>(generator);
+    ::binary::AddConverter<::binary::converters::LittleEndianConverter<int32_t>>(generator);
+    ::binary::AddConverter<::binary::converters::LittleEndianConverter<int64_t>>(generator);
+    ::binary::AddConverter<::binary::converters::StringConverter>(generator);
+    ::binary::AddConverter<::binary::converters::TupleConverter<T>>(generator);
+    auto converter = ::binary::GetConverter<T>(generator);
+    auto dataBuffer = ::binary::Allocator::Invoke([converter](auto& allocator) { converter->Encode(allocator, T()); });
+    auto autoBuffer = ::binary::Allocator::Invoke([converter](auto& allocator) { converter->EncodeAuto(allocator, T()); });
+    BOOST_REQUIRE_NE(autoBuffer.size(), 0);
+    BOOST_REQUIRE_LE(dataBuffer.size(), autoBuffer.size());
+
+    std::span<const std::byte> dataSpan(dataBuffer);
+    std::span<const std::byte> autoSpan(autoBuffer);
+    auto dataResult = converter->Decode(dataSpan);
+    auto autoResult = converter->DecodeAuto(autoSpan);
+    BOOST_REQUIRE(T() == dataResult);
+    BOOST_REQUIRE(T() == autoResult);
+    BOOST_REQUIRE_EQUAL(autoSpan.size(), 0);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 }
