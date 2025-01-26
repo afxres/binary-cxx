6b5b64c50e7d59c6e45700de5b909fa9a926e97b
miko <stdarg@outlook.com>
2025-01-26 21:53:03 +0800

    Fix tuple converter encode decode implementations

--- next/binary/include/binary/converters/TupleConverter.hpp
+++ next/binary/include/binary/converters/TupleConverter.hpp
@@ -15,67 +15,59 @@ template <template <typename...> typename T, typename... E>
     requires requires { std::tuple_size<T<E...>>::value; }
 class TupleConverter<T<E...>> : public Converter<T<E...>> {
 private:
-    using GenericArgument = T<E...>;
-    using ElementConverter = std::tuple<std::shared_ptr<Converter<std::remove_cv_t<E>>>...>;
-    static constexpr size_t TupleSize = std::tuple_size_v<GenericArgument>;
+    static constexpr size_t NoAutoLast = sizeof...(E) - 1;
+    static constexpr size_t AlwaysAuto = SIZE_MAX;
 
-    template <size_t Index, bool IsAuto = false>
-    struct Invoke {
-        static void Encode(const ElementConverter& converter, Allocator& allocator, const GenericArgument& item) {
-            if constexpr (Index == TupleSize - 1 && !IsAuto) {
-                std::get<Index>(converter)->Encode(allocator, std::get<Index>(item));
-            } else {
-                std::get<Index>(converter)->EncodeAuto(allocator, std::get<Index>(item));
-            }
-            if constexpr (Index != TupleSize - 1) {
-                Invoke<Index + 1, IsAuto>::Encode(converter, allocator, item);
-            }
+    template <size_t NoAutoIndex, size_t Index>
+    void EncodeAt(Allocator& allocator, const T<E...>& item) {
+        if constexpr (Index == NoAutoIndex) {
+            std::get<Index>(this->converter)->Encode(allocator, std::get<Index>(item));
+        } else {
+            std::get<Index>(this->converter)->EncodeAuto(allocator, std::get<Index>(item));
         }
-
-        static auto Detach(const ElementConverter& converter, std::span<const std::byte>& span) {
-            if constexpr (Index == TupleSize - 1 && !IsAuto) {
-                return std::get<Index>(converter)->Decode(span);
-            } else {
-                return std::get<Index>(converter)->DecodeAuto(span);
-            }
-        }
-
-        static auto Decode(const ElementConverter& converter, std::span<const std::byte>& span) {
-            auto result = std::make_tuple(Detach(converter, span));
-            if constexpr (Index != TupleSize - 1) {
-                return std::tuple_cat(result, Invoke<Index + 1, IsAuto>::Decode(converter, span));
-            } else {
-                return result;
-            }
-        }
-    };
-
-    static GenericArgument Result(std::remove_cv_t<E>&&... result) {
-        return {result...};
     }
 
-    ElementConverter converter;
+    template <size_t NoAutoIndex, size_t... Index>
+    void EncodeAll(Allocator& allocator, const T<E...>& item, std::index_sequence<Index...>) {
+        (EncodeAt<NoAutoIndex, Index>(allocator, item), ...);
+    }
+
+    template <size_t NoAutoIndex, size_t Index>
+    auto DecodeAt(std::span<const std::byte>& span) {
+        if constexpr (Index == NoAutoIndex) {
+            return std::get<Index>(this->converter)->Decode(span);
+        } else {
+            return std::get<Index>(this->converter)->DecodeAuto(span);
+        }
+    }
+
+    template <size_t NoAutoIndex, size_t... Index>
+    T<E...> DecodeAll(std::span<const std::byte>& span, std::index_sequence<Index...>) {
+        return {DecodeAt<NoAutoIndex, Index>(span)...};
+    }
+
+    std::tuple<std::shared_ptr<Converter<std::remove_cv_t<E>>>...> converter;
 
 public:
     TupleConverter(std::shared_ptr<Converter<std::remove_cv_t<E>>>... converter)
-        : Converter<GenericArgument>(internal::GetConverterLength(std::vector<std::shared_ptr<IConverter>>({converter...}) | std::views::transform([](const auto& converter) { return converter->Length(); })))
+        : Converter<T<E...>>(internal::GetConverterLength(std::vector<std::shared_ptr<IConverter>>({converter...}) | std::views::transform([](const auto& converter) { return converter->Length(); })))
         , converter({converter...}) {}
 
-    virtual void Encode(Allocator& allocator, const GenericArgument& item) override {
-        Invoke<0>::Encode(this->converter, allocator, item);
+    virtual void Encode(Allocator& allocator, const T<E...>& item) override {
+        EncodeAll<NoAutoLast>(allocator, item, std::make_index_sequence<sizeof...(E)>());
     }
 
-    virtual void EncodeAuto(Allocator& allocator, const GenericArgument& item) override {
-        Invoke<0, true>::Encode(this->converter, allocator, item);
+    virtual void EncodeAuto(Allocator& allocator, const T<E...>& item) override {
+        EncodeAll<AlwaysAuto>(allocator, item, std::make_index_sequence<sizeof...(E)>());
     }
 
-    virtual GenericArgument Decode(const std::span<const std::byte>& span) override {
+    virtual T<E...> Decode(const std::span<const std::byte>& span) override {
         std::span<const std::byte> copy = span;
-        return std::apply(Result, Invoke<0>::Decode(this->converter, copy));
+        return DecodeAll<NoAutoLast>(copy, std::make_index_sequence<sizeof...(E)>());
     }
 
-    virtual GenericArgument DecodeAuto(std::span<const std::byte>& span) override {
-        return std::apply(Result, Invoke<0, true>::Decode(this->converter, span));
+    virtual T<E...> DecodeAuto(std::span<const std::byte>& span) override {
+        return DecodeAll<AlwaysAuto>(span, std::make_index_sequence<sizeof...(E)>());
     }
 };
 }
