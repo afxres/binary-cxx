394889e9aff3e949043bc7aba5ac7f2f70991878
miko <stdarg@outlook.com>
2025-01-30 23:26:31 +0800

    Add unit tests for uncovered lines

--- next/binary-tests/src/binary/ConverterTests/CustomVariableConverterTests.cpp
+++ next/binary-tests/src/binary/ConverterTests/CustomVariableConverterTests.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <binary/Converter.hpp>
+#include <binary/ConverterExtensions.hpp>
 
 namespace tests::binary::ConverterTests {
 BOOST_AUTO_TEST_SUITE(CustomVariableConverterTests)
@@ -10,27 +11,60 @@ using ::binary::Allocator;
 using ::binary::Converter;
 
 class CustomVariableConverter : public Converter<int32_t> {
+public:
     virtual void Encode(Allocator& allocator, const int32_t& item) override {
-        allocator.Expand(static_cast<uint32_t>(item));
+        std::string source = std::to_string(item);
+        allocator.Append(std::as_bytes(std::span(source)));
     }
 
-    virtual int32_t Decode([[maybe_unused]] const std::span<const std::byte>& span) override {
-        throw std::logic_error("");
+    virtual int32_t Decode(const std::span<const std::byte>& span) override {
+        std::string source(reinterpret_cast<const char*>(span.data()), span.size());
+        return std::stoi(source);
     }
 };
 
-std::vector<std::tuple<int32_t, int32_t>> CustomVariableConverterEncodeAutoData = {
-    {0, 1},
-    {128, 132},
+BOOST_AUTO_TEST_CASE(CustomVariableConverterLengthTest) {
+    CustomVariableConverter converter;
+    BOOST_REQUIRE_EQUAL(converter.Length(), 0);
+}
+
+std::vector<std::tuple<int32_t, std::string>> CustomVariableConverterTestData = {
+    {0, "0"},
+    {128, "128"},
+    {65536, "65536"},
 };
 
-BOOST_DATA_TEST_CASE(CustomVariableConverterEncodeAutoTest, CustomVariableConverterEncodeAutoData, dataLength, outputLength) {
+BOOST_DATA_TEST_CASE(CustomVariableConverterBaseMethodsTest, CustomVariableConverterTestData, data, buffer) {
     CustomVariableConverter converter;
     Allocator allocator;
-    BOOST_REQUIRE_EQUAL(allocator.Length(), 0);
-    BOOST_REQUIRE_EQUAL(converter.Length(), 0);
-    converter.EncodeAuto(allocator, dataLength);
-    BOOST_REQUIRE_EQUAL(allocator.Length(), outputLength);
+    converter.Encode(allocator, data);
+    auto span = allocator.AsSpan();
+    BOOST_REQUIRE_EQUAL(buffer, std::string(reinterpret_cast<const char*>(span.data()), span.size()));
+    auto actual = converter.Decode(span);
+    BOOST_REQUIRE_EQUAL(actual, data);
+}
+
+BOOST_DATA_TEST_CASE(CustomVariableConverterAutoMethodsAndLengthPrefixMethodsTest, CustomVariableConverterTestData, data, buffer) {
+    CustomVariableConverter converter;
+    Allocator a;
+    Allocator b;
+    converter.EncodeAuto(a, data);
+    converter.EncodeWithLengthPrefix(b, data);
+    auto x = a.AsSpan();
+    auto y = b.AsSpan();
+    BOOST_REQUIRE_EQUAL(std::string(reinterpret_cast<const char*>(x.data()), x.size()), std::string(reinterpret_cast<const char*>(y.data()), y.size()));
+    auto i = converter.DecodeAuto(x);
+    auto j = converter.DecodeWithLengthPrefix(y);
+    BOOST_REQUIRE_EQUAL(data, i);
+    BOOST_REQUIRE_EQUAL(data, j);
+    BOOST_REQUIRE_EQUAL(0, x.size());
+    BOOST_REQUIRE_EQUAL(0, y.size());
+    auto actual = a.AsSpan();
+    size_t read;
+    size_t length = ::binary::Decode(actual, read);
+    BOOST_REQUIRE_EQUAL(length, buffer.size());
+    BOOST_REQUIRE_EQUAL(length + read, actual.size());
+    BOOST_REQUIRE_EQUAL(buffer, std::string(reinterpret_cast<const char*>(actual.data() + read), length));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- next/binary-tests/src/binary/converters/LittleEndianStringConverterTests/LittleEndianStringConverterTests.cpp
+++ next/binary-tests/src/binary/converters/LittleEndianStringConverterTests/LittleEndianStringConverterTests.cpp
@@ -20,9 +20,10 @@ std::vector<std::string> StringTestData = {
     std::string(),
     std::string("\0\x01\0z", 4),
     std::string("Hello, world!"),
+    std::string(127, 'A'),
 };
 
-BOOST_DATA_TEST_CASE(StringEncodeDecodeTest, StringTestData, source) {
+BOOST_DATA_TEST_CASE(LittleEndianStringConverterForStringEncodeDecodeTest, StringTestData, source) {
     auto a = ::binary::converters::LittleEndianStringConverter<std::string>();
     auto x = ::binary::Allocator::Invoke([&a, &source](auto& allocator) { a.Encode(allocator, source); });
     BOOST_REQUIRE_EQUAL(source, std::string(reinterpret_cast<const char*>(x.data()), x.size()));
@@ -30,13 +31,38 @@ BOOST_DATA_TEST_CASE(StringEncodeDecodeTest, StringTestData, source) {
     BOOST_REQUIRE_EQUAL(source, p);
 }
 
+BOOST_DATA_TEST_CASE(LittleEndianStringConverterForStringAutoMethodsAndLengthPrefixMethodsTest, StringTestData, source) {
+    auto converter = ::binary::converters::LittleEndianStringConverter<std::string>();
+    ::binary::Allocator a;
+    ::binary::Allocator b;
+    converter.EncodeAuto(a, source);
+    converter.EncodeWithLengthPrefix(b, source);
+    auto x = a.AsSpan();
+    auto y = b.AsSpan();
+    BOOST_REQUIRE_EQUAL(std::string(reinterpret_cast<const char*>(x.data()), x.size()), std::string(reinterpret_cast<const char*>(y.data()), y.size()));
+    auto i = converter.DecodeAuto(x);
+    auto j = converter.DecodeWithLengthPrefix(y);
+    BOOST_REQUIRE_EQUAL(source, i);
+    BOOST_REQUIRE_EQUAL(source, j);
+    BOOST_REQUIRE_EQUAL(0, x.size());
+    BOOST_REQUIRE_EQUAL(0, y.size());
+    auto actual = a.AsSpan();
+    size_t read;
+    size_t length = ::binary::Decode(actual, read);
+    BOOST_REQUIRE_EQUAL(read, 1);
+    BOOST_REQUIRE_EQUAL(length, source.size());
+    BOOST_REQUIRE_EQUAL(length + read, actual.size());
+    BOOST_REQUIRE_EQUAL(source, std::string(reinterpret_cast<const char*>(actual.data() + read), length));
+}
+
 std::vector<std::wstring> WideStringTestData = {
     std::wstring(),
     std::wstring(L"\0Z\0\x02", 4),
     std::wstring(L"Hello, world!"),
+    std::wstring(127 / sizeof(wchar_t), L'W'),
 };
 
-BOOST_DATA_TEST_CASE(WithStringEncodeDecodeTest, WideStringTestData, source) {
+BOOST_DATA_TEST_CASE(LittleEndianStringConverterForWideStringEncodeDecodeTest, WideStringTestData, source) {
     auto a = ::binary::converters::LittleEndianStringConverter<std::wstring>();
     auto x = ::binary::Allocator::Invoke([&a, &source](auto& allocator) { a.Encode(allocator, source); });
     BOOST_REQUIRE_EQUAL(source, std::wstring(reinterpret_cast<const wchar_t*>(x.data()), x.size() / sizeof(wchar_t)));
@@ -44,7 +70,31 @@ BOOST_DATA_TEST_CASE(WithStringEncodeDecodeTest, WideStringTestData, source) {
     BOOST_REQUIRE_EQUAL(source, p);
 }
 
-BOOST_AUTO_TEST_CASE(WideStringDecodeNotEnoughBytesTest) {
+BOOST_DATA_TEST_CASE(LittleEndianStringConverterForWideStringAutoMethodsAndLengthPrefixMethodsTest, WideStringTestData, source) {
+    auto converter = ::binary::converters::LittleEndianStringConverter<std::wstring>();
+    ::binary::Allocator a;
+    ::binary::Allocator b;
+    converter.EncodeAuto(a, source);
+    converter.EncodeWithLengthPrefix(b, source);
+    auto x = a.AsSpan();
+    auto y = b.AsSpan();
+    BOOST_REQUIRE_EQUAL(std::string(reinterpret_cast<const char*>(x.data()), x.size()), std::string(reinterpret_cast<const char*>(y.data()), y.size()));
+    auto i = converter.DecodeAuto(x);
+    auto j = converter.DecodeWithLengthPrefix(y);
+    BOOST_REQUIRE_EQUAL(source, i);
+    BOOST_REQUIRE_EQUAL(source, j);
+    BOOST_REQUIRE_EQUAL(0, x.size());
+    BOOST_REQUIRE_EQUAL(0, y.size());
+    auto actual = a.AsSpan();
+    size_t read;
+    size_t length = ::binary::Decode(actual, read);
+    BOOST_REQUIRE_EQUAL(read, 1);
+    BOOST_REQUIRE_EQUAL(length, source.size() * sizeof(wchar_t));
+    BOOST_REQUIRE_EQUAL(length + read, actual.size());
+    BOOST_REQUIRE_EQUAL(source, std::wstring(reinterpret_cast<const wchar_t*>(actual.data() + read), length / sizeof(wchar_t)));
+}
+
+BOOST_AUTO_TEST_CASE(LittleEndianStringConverterForWideStringDecodeNotEnoughBytesTest) {
     auto a = ::binary::converters::LittleEndianStringConverter<std::wstring>();
     std::string source = "abc";
     std::span<const std::byte> buffer(reinterpret_cast<const std::byte*>(source.data()), source.size());
