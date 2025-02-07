7294a1a9dd10bc5df6c54a05e778a6f0460b9194
miko <stdarg@outlook.com>
2025-02-07 23:04:56 +0800

    New converter create functions

--- next/binary/include/binary/GeneratorExtensions.hpp
+++ next/binary/include/binary/GeneratorExtensions.hpp
@@ -4,7 +4,7 @@
 #include "binary/Converter.hpp"
 #include "binary/IGenerator.hpp"
 #include "binary/internal/Converter.hpp"
-#include "binary/internal/GeneratorInsertFunction.hpp"
+#include "binary/internal/ConverterCreateFunction.hpp"
 
 namespace binary {
 template <typename T>
@@ -14,16 +14,17 @@ std::shared_ptr<Converter<T>> GetConverter(IGenerator& generator) {
 }
 
 template <typename TConverter, typename... TArguments>
-    requires std::derived_from<TConverter, IConverter> && std::constructible_from<TConverter, std::shared_ptr<Converter<TArguments>>...> && (sizeof...(TArguments) != 0)
+    requires std::derived_from<TConverter, IConverter> && std::constructible_from<TConverter, std::shared_ptr<Converter<TArguments>>...>
 void AddConverter(IGenerator& generator) {
     std::shared_ptr<IConverter> converter = std::make_shared<TConverter>(GetConverter<TArguments>(generator)...);
     generator.AddConverter(converter);
 }
 
 template <typename TConverter>
-    requires requires(IGenerator& generator) { internal::GeneratorInsertFunction<TConverter>()(generator); }
+    requires requires(IGenerator& generator) { internal::ConverterCreateFunction<TConverter>()(generator); }
 void AddConverter(IGenerator& generator) {
-    internal::GeneratorInsertFunction<TConverter>()(generator);
+    std::shared_ptr<IConverter> converter = internal::ConverterCreateFunction<TConverter>()(generator);
+    generator.AddConverter(converter);
 }
 }
 
---
+++ next/binary/include/binary/internal/ConverterCreateFunction.hpp
@@ -0,0 +1,40 @@
+#ifndef BINARY_INTERNAL_CONVERTERCREATEFUNCTION_HPP
+#define BINARY_INTERNAL_CONVERTERCREATEFUNCTION_HPP
+
+#include <concepts>
+#include <memory>
+#include <ranges>
+
+#include "binary/Converter.hpp"
+#include "binary/IGenerator.hpp"
+
+namespace binary::internal {
+template <typename TConverter>
+struct ConverterCreateFunction;
+
+template <typename TConverter>
+    requires std::derived_from<TConverter, IConverter> && std::constructible_from<TConverter, IGenerator&>
+struct ConverterCreateFunction<TConverter> {
+    auto operator()(IGenerator& generator) {
+        return std::make_shared<TConverter>(generator);
+    }
+};
+
+template <template <typename> typename TConverter, std::ranges::range TRange>
+    requires std::derived_from<TConverter<TRange>, IConverter> && std::constructible_from<TConverter<TRange>, std::shared_ptr<Converter<std::ranges::range_value_t<TRange>>>>
+struct ConverterCreateFunction<TConverter<TRange>> {
+    auto operator()(IGenerator& generator) {
+        return std::make_shared<TConverter<TRange>>(GetConverter<std::ranges::range_value_t<TRange>>(generator));
+    }
+};
+
+template <template <typename> typename TConverter, template <typename...> typename TTuple, typename... TArguments>
+    requires std::derived_from<TConverter<TTuple<TArguments...>>, IConverter> && std::constructible_from<TConverter<TTuple<TArguments...>>, std::shared_ptr<Converter<std::remove_cv_t<TArguments>>>...> && requires { std::tuple_size<TTuple<TArguments...>>::value; }
+struct ConverterCreateFunction<TConverter<TTuple<TArguments...>>> {
+    auto operator()(IGenerator& generator) {
+        return std::make_shared<TConverter<TTuple<TArguments...>>>(GetConverter<std::remove_cv_t<TArguments>>(generator)...);
+    }
+};
+}
+
+#endif
