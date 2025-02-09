a4dcce37ec8652fc55e049bafe11b543d1ddafb7
miko <stdarg@outlook.com>
2025-02-09 20:18:55 +0800

    Fix performance issues for container converter

--- next/binary/include/binary/internal/ContainerInsertFunction.hpp
+++ next/binary/include/binary/internal/ContainerInsertFunction.hpp
@@ -13,17 +13,17 @@ struct ContainerInsertFunction<T> {
 };
 
 template <std::ranges::input_range T>
-    requires requires(T& container, std::ranges::range_value_t<T>&& result) { container.emplace(result); }
+    requires requires(T& container, std::ranges::range_value_t<T>&& result) { container.emplace(std::move(result)); }
 struct ContainerInsertFunction<T> {
     static constexpr bool IsEnable = true;
-    void operator()(T& container, std::ranges::range_value_t<T>&& result) { container.emplace(result); }
+    void operator()(T& container, std::ranges::range_value_t<T>&& result) { container.emplace(std::move(result)); }
 };
 
 template <std::ranges::input_range T>
-    requires requires(T& container, std::ranges::range_value_t<T>&& result) { container.emplace_back(result); }
+    requires requires(T& container, std::ranges::range_value_t<T>&& result) { container.emplace_back(std::move(result)); }
 struct ContainerInsertFunction<T> {
     static constexpr bool IsEnable = true;
-    void operator()(T& container, std::ranges::range_value_t<T>&& result) { container.emplace_back(result); }
+    void operator()(T& container, std::ranges::range_value_t<T>&& result) { container.emplace_back(std::move(result)); }
 };
 }
 
