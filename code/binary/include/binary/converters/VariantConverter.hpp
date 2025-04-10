834444a816ec1e0c5fdf83ffed0c7db224c248a0
miko <stdarg@outlook.com>
2025-04-10 20:25:13 +0800

    Use functions for variant converter

--- code/binary/include/binary/converters/VariantConverter.hpp
+++ code/binary/include/binary/converters/VariantConverter.hpp
@@ -15,75 +15,85 @@ template <template <typename...> typename T, typename... E>
     requires requires { std::variant_size<T<E...>>::value; }
 class VariantConverter<T<E...>> : public Converter<T<E...>> {
 private:
-    using GenericArgument = T<E...>;
-    using ElementSequence = std::make_index_sequence<sizeof...(E)>;
+    using ConverterTargetType = T<E...>;
+    using EncodeFunction = std::function<void(Allocator&, const ConverterTargetType&)>;
+    using DecodeFunction = std::function<ConverterTargetType(std::span<const std::byte>&)>;
 
-    template <size_t IsAuto, size_t Index>
-    void EncodeInternal(Allocator& allocator, const GenericArgument& item) {
-        if (Index == item.index()) {
-            const auto& data = std::get<Index>(item);
-            const auto& converter = std::get<Index>(this->converter);
-            if constexpr (IsAuto == 0) {
-                converter->Encode(allocator, data);
-            } else {
-                converter->EncodeAuto(allocator, data);
-            }
-        }
+    struct MethodInfo {
+        EncodeFunction Encode;
+        EncodeFunction EncodeAuto;
+        DecodeFunction Decode;
+        DecodeFunction DecodeAuto;
+    };
+
+    template <size_t Index>
+    static MethodInfo GetMethodInfo(const auto& converter) {
+        MethodInfo result;
+        result.Encode = [converter](auto& allocator, const auto& item) { converter->Encode(allocator, std::get<Index>(item)); };
+        result.EncodeAuto = [converter](auto& allocator, const auto& item) { converter->EncodeAuto(allocator, std::get<Index>(item)); };
+        result.Decode = [converter](auto& span) { return ConverterTargetType(std::in_place_index<Index>, converter->Decode(span)); };
+        result.DecodeAuto = [converter](auto& span) { return ConverterTargetType(std::in_place_index<Index>, converter->DecodeAuto(span)); };
+        return result;
     }
 
-    template <size_t IsAuto, size_t... Index>
-    void EncodeInternal(Allocator& allocator, const GenericArgument& item, std::index_sequence<Index...>) {
-        if (item.valueless_by_exception()) {
+    template <size_t... Index>
+    static std::vector<MethodInfo> GetMethodInfoList(const std::shared_ptr<Converter<std::remove_cv_t<E>>>&... converter, std::index_sequence<Index...>) {
+        std::vector<MethodInfo> result;
+        (result.emplace_back(GetMethodInfo<Index>(converter)), ...);
+        return result;
+    }
+
+    template <size_t IsAuto>
+    void EncodeInternal(Allocator& allocator, const ConverterTargetType& item) {
+        auto header = item.index();
+        const auto& record = this->record;
+        if (header >= record.size()) {
             throw std::invalid_argument("invalid variant value");
         }
-        ::binary::Encode(allocator, item.index());
-        (EncodeInternal<IsAuto, Index>(allocator, item), ...);
-    }
 
-    template <size_t IsAuto, size_t Index>
-    auto DecodeInternal(std::span<const std::byte>& span, size_t index) {
-        if (Index == index) {
-            const auto& converter = std::get<Index>(this->converter);
-            if constexpr (IsAuto == 0) {
-                return GenericArgument(std::in_place_index<Index>, converter->Decode(span));
-            } else {
-                return GenericArgument(std::in_place_index<Index>, converter->DecodeAuto(span));
-            }
+        ::binary::Encode(allocator, header);
+        if constexpr (IsAuto == 0) {
+            record.at(header).Encode(allocator, item);
         } else {
-            if constexpr (Index == sizeof...(E) - 1) {
-                throw std::invalid_argument("invalid variant index");
-            } else {
-                return DecodeInternal<IsAuto, Index + 1>(span, index);
-            }
+            record.at(header).EncodeAuto(allocator, item);
         }
     }
 
     template <size_t IsAuto>
-    auto DecodeInternal(std::span<const std::byte>& span) {
-        auto index = ::binary::Decode(span);
-        return DecodeInternal<IsAuto, 0>(span, index);
+    ConverterTargetType DecodeInternal(std::span<const std::byte>& span) {
+        auto header = ::binary::Decode(span);
+        const auto& record = this->record;
+        if (header >= record.size()) {
+            throw std::invalid_argument("invalid variant index");
+        }
+
+        if constexpr (IsAuto == 0) {
+            return record.at(header).Decode(span);
+        } else {
+            return record.at(header).DecodeAuto(span);
+        }
     }
 
-    const std::tuple<std::shared_ptr<Converter<std::remove_cv_t<E>>>...> converter;
+    const std::vector<MethodInfo> record;
 
 public:
     VariantConverter(const std::shared_ptr<Converter<std::remove_cv_t<E>>>&... converter)
-        : converter({converter...}) {}
+        : record(GetMethodInfoList(converter..., std::make_index_sequence<sizeof...(E)>())) {}
 
-    virtual void Encode(Allocator& allocator, const GenericArgument& item) override {
-        EncodeInternal<0>(allocator, item, ElementSequence());
+    virtual void Encode(Allocator& allocator, const ConverterTargetType& item) override {
+        EncodeInternal<0>(allocator, item);
     }
 
-    virtual void EncodeAuto(Allocator& allocator, const GenericArgument& item) override {
-        EncodeInternal<1>(allocator, item, ElementSequence());
+    virtual void EncodeAuto(Allocator& allocator, const ConverterTargetType& item) override {
+        EncodeInternal<1>(allocator, item);
     }
 
-    virtual GenericArgument Decode(const std::span<const std::byte>& span) override {
+    virtual ConverterTargetType Decode(const std::span<const std::byte>& span) override {
         std::span<const std::byte> copy = span;
         return DecodeInternal<0>(copy);
     }
 
-    virtual GenericArgument DecodeAuto(std::span<const std::byte>& span) override {
+    virtual ConverterTargetType DecodeAuto(std::span<const std::byte>& span) override {
         return DecodeInternal<1>(span);
     }
 };
