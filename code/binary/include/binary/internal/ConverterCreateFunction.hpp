088f3d447f52cd06b009407c51d304bfc3367e2a
miko <stdarg@outlook.com>
2025-05-03 10:17:07 +0800

    Reduce converter create functions

--- code/binary/include/binary/internal/ConverterCreateFunction.hpp
+++ code/binary/include/binary/internal/ConverterCreateFunction.hpp
@@ -4,7 +4,6 @@
 #include <concepts>
 #include <memory>
 #include <ranges>
-#include <variant>
 
 #include "binary/Converter.hpp"
 #include "binary/IGenerator.hpp"
@@ -30,23 +29,12 @@ struct ConverterCreateFunction<TConverter<TRange>> {
     }
 };
 
-template <template <typename> typename TConverter, template <typename...> typename TTuple, typename... TArguments>
-    requires std::derived_from<TConverter<TTuple<TArguments...>>, IConverter> &&
-    std::constructible_from<TConverter<TTuple<TArguments...>>, const std::shared_ptr<Converter<std::remove_cv_t<TArguments>>>&...> &&
-    requires { std::tuple_size<TTuple<TArguments...>>::value; }
-struct ConverterCreateFunction<TConverter<TTuple<TArguments...>>> {
+template <template <typename> typename TConverter, template <typename...> typename TValue, typename... TArguments>
+    requires std::derived_from<TConverter<TValue<TArguments...>>, IConverter> &&
+    std::constructible_from<TConverter<TValue<TArguments...>>, const std::shared_ptr<Converter<std::remove_cv_t<TArguments>>>&...>
+struct ConverterCreateFunction<TConverter<TValue<TArguments...>>> {
     auto operator()(const IGenerator& generator) {
-        return std::make_shared<TConverter<TTuple<TArguments...>>>(GetConverter<std::remove_cv_t<TArguments>>(generator)...);
-    }
-};
-
-template <template <typename> typename TConverter, template <typename...> typename TVariant, typename... TArguments>
-    requires std::derived_from<TConverter<TVariant<TArguments...>>, IConverter> &&
-    std::constructible_from<TConverter<TVariant<TArguments...>>, const std::shared_ptr<Converter<std::remove_cv_t<TArguments>>>&...> &&
-    requires { std::variant_size<TVariant<TArguments...>>::value; }
-struct ConverterCreateFunction<TConverter<TVariant<TArguments...>>> {
-    auto operator()(const IGenerator& generator) {
-        return std::make_shared<TConverter<TVariant<TArguments...>>>(GetConverter<std::remove_cv_t<TArguments>>(generator)...);
+        return std::make_shared<TConverter<TValue<TArguments...>>>(GetConverter<std::remove_cv_t<TArguments>>(generator)...);
     }
 };
 }
