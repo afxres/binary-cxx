2aea3b1f568b0c4e6b62e6b1097b7dc738e15d6a
miko <stdarg@outlook.com>
2025-12-09 21:19:14 +0800

    Update build script

--- scripts/build-tests.sh
+++ scripts/build-tests.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-PROJECT_ROOT="$PWD"
-PROJECT_DIR="$PROJECT_ROOT/code"
-BUILD_DIR="$PROJECT_ROOT/build-tests"
-
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
-cmake -DCMAKE_BUILD_TYPE=Debug -DBINARY_BUILD_TESTS=ON "$PROJECT_DIR"
-cmake --build . --parallel
-ctest --output-on-failure
+"$(cd "$(dirname "$0")" && pwd)/build-tests-multiarch.sh" "$(uname -m)"
